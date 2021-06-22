63337f49a2bb067a174b7530e0e65439c46eb624
Nikhil Kathal <niks@nmac.lan>
2021-06-22 02:45:55 -0400

    implemented vote counts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,8 @@
 import os
 import csv
+import collections
 
-budget_csv = os.path.join ( 'Resources', 'election_data.csv')
+election_data = os.path.join ( 'Resources', 'election_data.csv')
 
 # Tasks;
 # 1. The total number of votes cast
@@ -9,3 +10,35 @@ budget_csv = os.path.join ( 'Resources', 'election_data.csv')
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
+
+with open(election_data) as pollFile:
+    csv_reader = csv.reader (pollFile, delimiter=',')
+    num_rows = 0
+    candidate = []
+    all_votes = []
+
+    for row in csv_reader:
+        if num_rows > 0:
+            all_votes.append(row[2])
+            if row[2] not in candidate:
+                candidate.append(row[2])
+
+        num_rows += 1
+    
+    #from collections import Counter
+    counter = collections.Counter (all_votes)
+
+    print(f'Election Results')
+    print(f'-------------------------')
+    print(f'Total Votes: {num_rows - 1}')
+    print(f'-------------------------')    
+    print(f'Candidates: {candidate}')
+    print(f'all votes: {len(all_votes)}')
+    print(f'all candidate: {len(candidate)}')
+    print(f'Counter: {counter}')
+
+    for person in counter:
+        key = person
+        value = counter[key]
+        percent = int(value) / len(all_votes)
+        print(f'{key}: {percent} ({value})')
