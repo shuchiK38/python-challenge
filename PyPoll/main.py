a08bdc21a90c356a051d8bf4ef656f1f3c144517
Nikhil Kathal <niks@nmac.lan>
2021-06-22 01:35:25 -0400

    PyPoll initial file

---
+++ PyPoll/main.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+budget_csv = os.path.join ( 'Resources', 'election_data.csv')
+
+# Tasks;
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
